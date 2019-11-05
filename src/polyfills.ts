d47846489de00d711dad3aa512a1be2bdac23d2e
Daniel <danielhcomerio@gmail.com>
2019-11-05 19:58:29 -0200

    Commit 05/11/2019

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,3 +61,8 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+
+ 
+//import 'intl'
+//import 'intl/locale-data/jsonp/pt-Br';
