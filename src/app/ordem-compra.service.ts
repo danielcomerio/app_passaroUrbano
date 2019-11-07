2b1fd1197625acbaa822e08f52fa319efaada7c8
Daniel <danielhcomerio@gmail.com>
2019-11-07 19:59:09 -0200

    Commit 07/11/2019

---
+++ src/app/ordem-compra.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core'
+import { Http, RequestOptions, Headers } from '@angular/http'
+import { Pedido } from './shared/pedido.model'
+import { Observable } from 'rxjs'
+
+import { URL_API } from './app.api'
+
+
+@Injectable()
+export class OrdemCompraService {
+
+    constructor(private http: Http) {}
+
+    public efetivarCompra(pedido: Pedido): Observable<any> {
+        
+        let headers: Headers = new Headers()
+
+        headers.append('Content-type', 'application/json')
+
+        return this.http.post(
+            `${URL_API}/pedidos`,
+            JSON.stringify(pedido),
+            new RequestOptions({ headers: headers })
+        )
+    }
+
+}
